67a6cc3ed680aba83b594ce5c7e9dff40280c4b8
triastanto <rahmadhany.triastanto@gmail.com>
2024-01-09 14:52:29 +0700

    putting the list using card

--- lib/main.dart
+++ lib/main.dart
@@ -58,9 +58,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index) {
-              // TODO: Update to return Recipe Card
-              return Text(Recipe.samples[index].label);
+              return buildRecipeCard(Recipe.samples[index]);
             }),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          Text(recipe.label),
+        ],
       ),
     );
   }
